acc0708b8de7ea4d906c788eaec22b611262778e
Simone Susino <simworld@tuta.io>
2023-11-07 14:27:38 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+  );
+}
+
+export default App;
